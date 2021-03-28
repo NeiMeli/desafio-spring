63944356adbb81319cab4bf5d1ea5d2a72ff775f
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-03-28 09:59:40 -0300

    first develop commit

--- src/main/java/com/bootcamp/springchallenge/controller/common/util/DoubleProcessor.java
+++ src/main/java/com/bootcamp/springchallenge/controller/common/util/DoubleProcessor.java
@@ -17,6 +17,7 @@ public class DoubleProcessor {
     }
 
     public static double roundTwoDecimals(double d) {
+
         BigDecimal bd = new BigDecimal(Double.toString(d));
         return bd.setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
